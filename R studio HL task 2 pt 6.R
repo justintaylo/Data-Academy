d4a1392fab90b6d6787638305465dd41490ca6f0
Justintaylo <87228963+justintaylo@users.noreply.github.com>
2021-10-12 22:47:01 +0100

    Add files via upload
    R Studio HL

---
+++ R studio HL task 2 pt 6.R
@@ -0,0 +1,6 @@
+print("Sequence of numbers from 20 to 50:");
+print(seq(20,50));
+print("Mean of numbers from 20 to 50:");
+print(mean(20:50));
+print("Sum of numbers from 20 to 50:");
+print(sum(20:50))
