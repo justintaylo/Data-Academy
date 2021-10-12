6c75724d1c1bbbead2d54ebae4953e6cde952242
Justintaylo <87228963+justintaylo@users.noreply.github.com>
2021-10-12 22:47:21 +0100

    Add files via upload
    R Studio HL

---
+++ R studio task 2 part 2 and 3.R
@@ -0,0 +1,11 @@
+> name <- c('Jane','Peter','John','helen',"sophie")
+> age <- c(21, 23, 26 ,32, 33)
+> role <- c('engineer', 'team lead', 'delivery manager', 'engineer', 'engineer' )
+> los <- c(2, 3, 4 ,5, 1)
+employ.data <- data.frame(name, age, role,los
+print employ.data
+install.packages("ggplot2")
+library(ggplot2)
+data(package = "ggplot2")
+ggplot2::mpg
+qplot(data 
