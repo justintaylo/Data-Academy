d147f1d774e41b2601d2708cec57dcf0c6aeb291
Justintaylo <87228963+justintaylo@users.noreply.github.com>
2021-10-12 22:46:50 +0100

    Add files via upload
    R studio HL

---
+++ R studio HL task 2 prt 5.R
@@ -0,0 +1,6 @@
+my.name <- readline(prompt="Enter name: ");
+my.age <- readline(prompt="Enter age: ");
+my.age <- as.integer(my.age);
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
+
+
